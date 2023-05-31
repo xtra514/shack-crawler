968fb78ab2222bfe72412e385ed54ad5c0db9daa
xtra514 <96725113+xtra514@users.noreply.github.com>
2023-05-31 23:32:32 +0530

    Update refresh.ts

--- src/refresh.ts
+++ src/refresh.ts
@@ -1,53 +1,6 @@
 require("dotenv").config();
 
 import { load } from "cheerio";
-import axios from "axios";
-import { Collection, MongoClient, MongoClientOptions } from "mongodb";
-
-import { logger, colors } from "./utils/utils";
-import { scrapeRecentRelease, recent_release_url, getCollection, connectToDB } from "./helpers";
-import { Types, Gogoanime } from "./models";
-import { validateGogoanimeV2 } from "./resolvers/validate-gogoanime";
-
-let mongoClient: MongoClient;
-let gogoanimeColl: Collection<Gogoanime>;
-
-let startTime: number, endTime: number;
-global.config = {
-  animesAdded: 0,
-  animesUpdated: 0,
-};
-
-const startRefresh = async () => {
-  logger.info(`\nStarting refreshing crawler... PID:${process.pid}`, colors.blue);
-
-  gogoanimeColl = getCollection<Gogoanime>(mongoClient, process.env.DB_NAME!, "gogoanime")!;
-
-  try {
-    for (const type of [Types.SUB, Types.DUB, Types.CHINESE]) {
-      let page = 1;
-
-      await handlePages(type, page);
-
-      logger.info(`\nFinished scraping type ${type}...`, colors.green);
-    }
-
-    logger.info(
-      `\nFinished refreshing process. PID:${process.pid}, will start refreshing again right now.`,
-      colors.green
-    );
-  } catch (err) {
-    throw new Error((err as Error).message);
-  }
-};
-
-const handlePages = async (type: number, page: number) => {
-  const url = `${recent_release_url}?page=${page}&type=${type}`;
-
-  logger.info(`\nScraping type ${type}, page ${page}...`, colors.blue);
-
-  const html = await axios.get(url);
-  const $ = load(html.data);
 
   const hasNextPage = page < 10;
 
@@ -89,13 +42,4 @@ const validateEnviromentVariables = () => {
 };
 
 (async () => {
-  logger.info(`\nStarting refreshing process...`, colors.blue);
-
-  validateEnviromentVariables();
-  startTime = performance.now();
-
-  await connectToDB(mongoClient!);
-  while (true) {
-    await startRefresh();
-  }
-})();
+  logger.info
